0272c6b5405ef081e96ad61bac83045fd88b9383
amedouar <abdes_69@hotmail.com>
2015-09-25 21:50:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,42 +6,60 @@ counter();
 learnMore();
 orderNow();
 logIn();
-
+profileCall();
 };
 
 function menuSlide() {
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').toggle(
     function() {
-      $('.menu').animate({left: "0px"}, 100);
-      $('body').animate({left: "285px"}, 100);
+      $('.menu').animate({left: "0"}, 100);
+      $('body').animate({left: "19em"}, 100);
       $('.bottom').hide();
     },
     function() {
-      $('.menu').animate({left: "-285px"}, 100);
-      $('body').animate({left: "0px"}, 100);
+      $('.menu').animate({left: "-19em"}, 100);
+      $('body').animate({left: "0"}, 100);
       $('.bottom').show();
     });
 
   /* Then push them back */
   $('body').click(function() {
-    $('.menu').animate({left: "-285px"}, 100);
-    $('body').animate({left: "0px"}, 100);
+    $('.menu').animate({left: "-19em"}, 100);
+    $('body').animate({left: "0"}, 100);
     $('.bottom').show();
   });
+  $('.menu').click(function(event){
+      event.stopPropagation();
+      if($(event.target).is('#profile')){
+        $('.profileContainer').show();
+        $('.menu').animate({left: "-19em"}, 100);
+        $('body').animate({left: "0"}, 100);
+      };
+
+  $('body').click(function() {
+    $('.profileContainer').hide();
+    $('.bottom').show();
+  });
+  /*$('.profileContainer').click(function(event)){
+    event.stopPropagation();
+  };*/
+  stopProp('.profileContainer');
+
+    });
 };
 
 /* Defining the counter*/
 function counter() {
-	var num;
-	/* When clicking +*/
-  $("#countUp1").click(function(){
+  var num;
+  /* When clicking +*/
+  $("#increaseCount1").click(function(){
       /* Take the number value in the counter and increase it by 1*/
-    	num = parseInt($("#count1").text(),10) + 1;
-    	$("#count1").text(num);
+      num = parseInt($("#count1").text(),10) + 1;
+      $("#count1").text(num);
     });
     /*When clicking -*/
-  $('#countDown1').click(function(){
+  $('#decreaseCount1').click(function(){
     /* Take the base 10 number in the counter*/
     num = parseInt($("#count1").text(),10);
     if(num>0){
@@ -52,13 +70,13 @@ function counter() {
   });
 
 
-  $("#countUp2").click(function(){
+  $("#increaseCount2").click(function(){
       /* Take the number value in the counter and increase it by 1*/
       num = parseInt($("#count2").text(),10) + 1;
       $("#count2").text(num);
     });
     /*When clicking -*/
-  $('#countDown2').click(function(){
+  $('#decreaseCount2').click(function(){
     /* Take the base 10 number in the counter*/
     num = parseInt($("#count2").text(),10);
     if(num>0){
@@ -69,13 +87,13 @@ function counter() {
   });
 
 
-  $("#countUp3").click(function(){
+  $("#increaseCount3").click(function(){
       /* Take the number value in the counter and increase it by 1*/
       num = parseInt($("#count3").text(),10) + 1;
       $("#count3").text(num);
     });
     /*When clicking -*/
-  $('#countDown3').click(function(){
+  $('#decreaseCount3').click(function(){
     /* Take the base 10 number in the counter*/
     num = parseInt($("#count3").text(),10);
     if(num>0){
@@ -113,4 +131,28 @@ function orderNow(){
     $('.container').click(function(event){
       event.stopPropagation();
     });
+};
+
+function profileCall(){
+    $('.profiletested').click(function(){
+        $('.profileContainer').show();
+        alert("sa marche");
+      } 
+      /*function(){
+        $('.profileContainer').hide();
+      }*/
+    );
+    $('body').click(function(){
+      $('.profileContainer').hide();
+    });
+    $('.profileContainer').click(function(event){
+      event.stopPropagation();
+    });
+    
+};
+
+function stopProp(a){
+  $(a).click(function(event){
+    event.stopPropagation();
+  });
 };
